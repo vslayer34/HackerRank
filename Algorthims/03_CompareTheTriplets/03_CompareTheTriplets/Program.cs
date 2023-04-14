05423a287d5e6318d41725e5b84763869e4b489a
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-14 11:08:19 +0200

    Solved Compare the Triplets

--- Algorthims/03_CompareTheTriplets/03_CompareTheTriplets/Program.cs
+++ Algorthims/03_CompareTheTriplets/03_CompareTheTriplets/Program.cs
@@ -11,6 +11,23 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+using System.Drawing;
+using System.Reflection.Metadata;
+
+// URL: https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
+
+/*Alice and Bob each created one problem for HackerRank.
+ * A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty. 
+ * The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+ * The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2]. 
+ *  
+ *  If a[i] > b[i], then Alice is awarded 1 point. 
+ *  If a[i] < b[i], then Bob is awarded 1 point. 
+ *  If a[i] = b[i], then neither person receives a point.
+ *  
+ *  Comparison points is the total points a person earned. 
+ *  Given a and b, determine their respective comparison points. 
+ */
 
 class Result
 {
@@ -26,26 +43,73 @@ class Result
 
     public static List<int> compareTriplets(List<int> a, List<int> b)
     {
+        List<int> finalScore = new List<int>(){ 0, 0 };
 
+        for (int i = 0; i < a.Count; i++)
+        {
+            if (a[i] > b[i])
+                finalScore[0]++;
+            else if (a[i] < b[i])
+                finalScore[1]++;
+        }
+
+        return finalScore;
     }
-
 }
 
 class Solution
 {
     public static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        // Example 1
+        {
+            // a = [1, 2, 3]
+            // b = [3, 2, 1] 
+            // For elements *0*, Bob is awarded a point because a[0].
+            // For the equal elements a[1] and b[1], no points are earned.
+            // Finally, for elements 2, a[2] > b[2] so Alice receives a point. 
 
-        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+            // The return array is [1, 1] with Alice's score first and Bob's second. 
 
-        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+            List<int> a = new List<int>(){ 1, 2, 3 };
+            List<int> b = new List<int>() { 3, 2, 1 };
+            var result = Result.compareTriplets(a, b);
 
-        List<int> result = Result.compareTriplets(a, b);
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+        }
+        Console.WriteLine(new string('-', 100));
 
-        textWriter.WriteLine(String.Join(" ", result));
+        // sample input and output 0
+        {
+            // a = [5, 6, 7]
+            // b = [3, 6, 10]
 
-        textWriter.Flush();
-        textWriter.Close();
+            // output = [1, 1]
+
+            List<int> a = new List<int>() { 5, 6, 7 };
+            List<int> b = new List<int>() { 3, 6, 10 };
+            var result = Result.compareTriplets(a, b);
+
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+        }
+        Console.WriteLine(new string('-', 100));
+
+
+        // sample input and output 1
+        {
+            // a = [17, 28, 30]
+            // b = [99, 16, 8]
+
+            // output = [2, 1]
+            List<int> a = new List<int>() { 17, 28, 30 };
+            List<int> b = new List<int>() { 99, 16, 8 };
+            var result = Result.compareTriplets(a, b);
+
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+        }
+        Console.WriteLine(new string('-', 100));
     }
 }
