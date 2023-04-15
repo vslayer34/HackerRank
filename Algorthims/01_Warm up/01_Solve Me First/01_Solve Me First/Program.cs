14947a8f59c34c0483b06de348cc4a620bc5f076
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-15 08:07:42 +0200

    Solved 1. Solve me first

---
+++ Algorthims/01_Warm up/01_Solve Me First/01_Solve Me First/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class Solution
+{
+    static int solveMeFirst(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello G5 Laptop");
+    }
+}
