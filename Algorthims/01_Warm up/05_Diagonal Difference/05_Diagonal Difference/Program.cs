2460e1bde1f982c8aff88c196df87fbb78e1e2e7
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-17 08:35:02 +0200

    Solved 06_Plus Minus in Algorthims/01_Warm up/

---
+++ Algorthims/01_Warm up/05_Diagonal Difference/05_Diagonal Difference/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+internal class Result
+{
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        // two sums for each diagonal
+        int sum1 = 0;
+        int sum2 = 0;
+
+        // index to iterate through each list
+        int leftToRightIndex = 0;                   // starts from the bigining
+        int rightToLeftIndex = arr.Count - 1;       // starts from the end
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            sum1 += arr[i][leftToRightIndex];
+            sum2 += arr[i][rightToLeftIndex];
+            leftToRightIndex++;
+            rightToLeftIndex--;
+        }
+        
+        return Math.Abs(sum1 - sum2);
+    }
+
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        {
+            // input =
+            // 11   2     4
+            // 4    5     6
+            // 10   8    -12
+
+            // output = 15
+
+            List<List<int>> input = new List<List<int>>()
+            {
+                new List<int>() { 11, 2, 4 },
+                new List<int>() { 4, 5, 6 },
+                new List<int>() { 10, 8, -12 }
+            };
+
+            Helper.DisplayExample(
+                "11   2     4\n" +
+                "4    5     6\n" +
+                "10   8    -12\n",
+                output: "15");
+            Console.WriteLine(Result.diagonalDifference(input));
+        }
+        Console.WriteLine(new string('-', 100));
+    }
+}
---
+++ Algorthims/01_Warm up/06_Plus Minus/06_Plus Minus/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+internal class Result
+{
+    public static void plusMinus(List<int> arr)
+    {
+        // counter for the 3 states
+        int positiveNumbers = 0;
+        int negativeNumbers = 0;
+        int zeros = 0;
+
+        // iratate through the list and add numbers to each counter
+        foreach (int num in arr)
+        {
+            if (num > 0)
+                positiveNumbers++;
+
+            else if (num < 0)
+                negativeNumbers++;
+
+            else
+                zeros++;
+        }
+
+        float arrCount = arr.Count;
+
+        Console.WriteLine($"{(positiveNumbers / arrCount).ToString("F6")}\n" +
+                          $"{(negativeNumbers / arrCount).ToString("F6")}\n" +
+                          $"{(zeros / arrCount).ToString("F6")}");
+    }
+
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        {
+            // array size: 6
+            // array elements: { -4, 3, -9, 0, 4, 1 }
+            List<int> list = new List<int>() { -4, 3, -9, 0, 4, 1 };
+
+            // output =
+            // 0.500000
+            // 0.333333
+            // 0.166667
+
+            Helper.DisplayExample("array size: 6\n" +
+                "array elements: { -4, 3, -9, 0, 4, 1 }",
+                "0.500000\n\t\t0.333333\n\t\t0.166667\n");
+
+            Result.plusMinus(list);
+        }
+        Console.WriteLine(new string('-', 100));
+
+
+
+        // Example 2
+        {
+            // array size: 8
+            // array elements: { 1, 2, 3, -1, -2, -3, 0, 0 }
+            List<int> list = new List<int>() { 1, 2, 3, -1, -2, -3, 0, 0 };
+
+            // output =
+            // 0.375000
+            // 0.375000
+            // 0.250000
+
+            Helper.DisplayExample("array size: 8\n" +
+                "array elements: { 1, 2, 3, -1, -2, -3, 0, 0 }",
+                "0.375000\n\t\t0.375000\n\t\t0.250000\n");
+
+            Result.plusMinus(list);
+        }
+        Console.WriteLine(new string('-', 100));
+    }
+}
