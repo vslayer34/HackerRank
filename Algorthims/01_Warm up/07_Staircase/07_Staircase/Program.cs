74569c54f9d91e4ae0ef48248d24b03bc48ce572
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-18 05:49:29 +0200

    Solved 07_Staircase in algorithms\01_Warm up

---
+++ Algorthims/01_Warm up/07_Staircase/07_Staircase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Result
+{
+    public static void staircase(int size)
+    {
+        int whiteSpaces = size - 1;
+        for (int i = 1; i <= size; i++)
+        {
+            Console.Write(new string(' ', whiteSpaces));
+            Console.WriteLine(new string('#', i));
+            whiteSpaces--;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        {
+            // input = 6
+            int input = 6;
+
+            Helper.DisplayExample("Staircase size: 6",
+                "\n     #\n" +
+                "    ##\n" +
+                "   ###\n" +
+                "  ####\n" +
+                " #####\n" +
+                "######\n");
+
+            Console.WriteLine();
+            Result.staircase(input);
+        }
+        Console.WriteLine(new string('-', 100));
+    }
+}
