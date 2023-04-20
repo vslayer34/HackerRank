8fde7380e3024e3c450a5e790fafc80d5287a247
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-20 12:07:28 +0200

    Solved 09_Birthday Cake Candles in Algorithms/01_Warm up

---
+++ Algorthims/01_Warm up/09_Birthday Cake Candles/09_Birthday Cake Candles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+internal class Result
+{
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int numOfTallestCandles = 0;
+        int tallestCandle = candles.Max();
+
+        foreach (int candle in candles)
+        {
+            if (candle >= tallestCandle)
+                numOfTallestCandles++;
+        }
+
+        return numOfTallestCandles;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        {
+            // Input = {3, 2, 1, 3}
+            // output = 2
+            List<int> inputs = new List<int>() { 3, 2, 1, 3 };
+
+            Helper.DisplayExample("Input = {3, 2, 1, 3}\n", "2");
+            Console.WriteLine(Result.birthdayCakeCandles(inputs));
+        }
+        Console.WriteLine(new string('-', 100));
+    }
+}
