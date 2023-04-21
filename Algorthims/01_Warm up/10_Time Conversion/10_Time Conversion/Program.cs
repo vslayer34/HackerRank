7b9c544fb152c55fc37dbac2b34929283e8ab74c
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-21 09:50:27 +0200

    Solved 10_Time Conversion in Algorithms/01_Warm up

---
+++ Algorthims/01_Warm up/10_Time Conversion/10_Time Conversion/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+internal class Result
+{
+    public static string timeConversion(string s)
+    {
+        DateTime date = DateTime.Parse(s);
+
+        return date.ToString("HH:mm:ss");
+    }
+
+
+    //*************************************************************************************************************
+    public static void Main(string[] args)
+    {
+        // Example 1
+        {
+            // input: "12:01:00PM"
+            // output: "12:01:00"
+            string time = "12:01:00PM";
+
+            Helper.DisplayExample(time + '\n', "12:01:00");
+            Console.WriteLine(Result.timeConversion(time));
+        }
+        Console.WriteLine(new string('-', 100));
+
+
+
+        // Example 2
+        {
+            // input: "12:01:00AM"
+            // output: "00:01:00"
+            string time = "12:01:00AM";
+
+            Helper.DisplayExample(time + '\n', "00:01:00");
+            Console.WriteLine(Result.timeConversion(time));
+        }
+        Console.WriteLine(new string('-', 100));
+
+
+
+        // Example 3
+        {
+            // input: "07:05:45PM"
+            // output: "19:05:45"
+            string time = "07:05:45PM";
+
+            Helper.DisplayExample(time + '\n', "19:05:45");
+            Console.WriteLine(Result.timeConversion(time));
+        }
+        Console.WriteLine(new string('-', 100));
+    }
+}
