5e3612f632b23da3db3c8045d4349ca762c6575a
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-24 15:38:58 +0200

    Solved 05_Breaking the Records in Algorithms/02_Implementation

---
+++ Algorithms/02_Implementation/04_Between Two Sets/04_Between Two Sets/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello G5 Laptop");
+    }
+}
---
+++ Algorithms/02_Implementation/05_Breaking the Records/05_Breaking the Records/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+internal class Result
+{
+    public static List<int> breakingRecords(List<int> scores)
+    {
+        int lowestScore = scores[0];
+        int highestScore = scores[0];
+
+        // counter for highest and lowest score record broken
+        int lowestRecord = 0;
+        int highestRecord = 0;
+
+        foreach (int score in scores)
+        {
+            if (score < lowestScore)
+            {
+                lowestScore = score;
+                lowestRecord++;
+            }
+
+            if (score > highestScore)
+            {
+                highestScore = score;
+                highestRecord++;
+            }
+        }
+
+        return new List<int>() { highestRecord, lowestRecord };
+    }
+
+    //****************************************************************************************
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        {
+            // input number: 9
+            // input List" { 10, 5, 20, 20, 4, 5, 2, 25, 1 }
+            // first number represents number of highest score broken record
+            // second number represents number of lowest score broken record
+            // output: 2 4
+
+            List<int> score = new List<int>(9) { 10, 5, 20, 20, 4, 5, 2, 25, 1 };
+
+            Helper.DisplayExample(
+                "Matches: 9\n" +
+                "Scores : { 10, 5, 20, 20, 4, 5, 2, 25, 1 }\n",
+                "2 4");
+            Console.Write(Result.breakingRecords(score)[0] + " ");
+            Console.WriteLine(Result.breakingRecords(score)[1]);
+        }
+        Console.WriteLine(new string('-', 100));
+
+
+
+        // Example 2
+        {
+            // input number: 10
+            // input List" { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 }
+            // first number represents number of highest score broken record
+            // second number represents number of lowest score broken record
+            // output: 2 4
+
+            List<int> score = new List<int>(10) { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 };
+
+            Helper.DisplayExample(
+                "Matches: 10\n" +
+                "Scores : { 3, 4, 21, 36, 10, 28, 35, 5 , 24, 42 }\n",
+                "4 0");
+            Console.Write(Result.breakingRecords(score)[0] + " ");
+            Console.WriteLine(Result.breakingRecords(score)[1]);
+        }
+        Console.WriteLine(new string('-', 100));
+    }
+}
