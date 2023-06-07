96a4fb7b20503b747d9c1d3a3ed0affeb7a004e7
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-06-07 05:26:04 +0300

    prepeared 11_Sales by Match in Algorithms/02_Implementation

---
+++ Algorithms/02_Implementation/11_Sales by Match/11_Sales by Match/Result.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+internal class Result
+{
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        return 0;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        {
+            // number of socks = 7
+            // list of socks: { 1, 2, 1, 2, 1, 3, 2 }
+            // output: 2
+
+            int numOfSocks = 7;
+            List<int> listOfSocks = new List<int>(numOfSocks) { 1, 2, 1, 2, 1, 3, 2 };
+
+            Helper.DisplayExample(
+                $"Number of socks: {numOfSocks}\n" +
+                "List of Socks: { 1, 2, 1, 2, 1, 3, 2 }\n",
+                2.ToString());
+
+            Console.WriteLine(sockMerchant(numOfSocks, listOfSocks));
+        }
+        Console.WriteLine(new string('-', 100));
+
+
+
+        // Example 2
+        {
+            // number of socks = 9
+            // list of socks: { 10, 20, 20, 10, 10, 30, 50, 10, 20 }
+            // output: 3
+
+            int numOfSocks = 9;
+            List<int> listOfSocks = new List<int>(numOfSocks) { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+
+            Helper.DisplayExample(
+                $"Number of socks: {numOfSocks}\n" +
+                "List of Socks: { 10, 20, 20, 10, 10, 30, 50, 10, 20 }\n",
+                3.ToString());
+
+            Console.WriteLine(sockMerchant(numOfSocks, listOfSocks));
+        }
+        Console.WriteLine(new string('-', 100));
+    }
+}
